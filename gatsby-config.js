f4932ec4442b54863dc9b07d6b6fc4ee388878c2
Alexander Kiefer <alexanderkiefer@Alexanders-MacBook-Pro.local>
2020-04-08 18:51:54 +0200

    Added gatsby-sharp-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
